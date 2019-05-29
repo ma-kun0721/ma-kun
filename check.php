d6a4050260a65cc2dc7d9152caf0977366ebfb0a
古市仁彦 <zhengyangushi@gmail.com>
2019-05-29 14:57:35 +0800

    check.phpを

---
+++ check.php
@@ -0,0 +1,42 @@
+<?php 
+$nickname = $_POST['nickname'];
+// スーパーグローバル関数
+$nickname = $_POST['nickname'];
+$email = $_POST['email'];
+$content = $_POST['content'];
+// var_dump($_POST);
+// .echo $nickname;
+if($nickname == '') {
+        $nickname_result = 'ニックネームが入力されてません。';
+    } else {
+        $nickname_result = 'ようこそ、' . $nickname . '様';
+    }
+
+    if ($email == '') {
+        $email_result = 'メールアドレスが入力されてません。';
+    } else {
+        $email_result = 'メールアドレス:' . $email;
+    }
+
+    if ($content == '') {
+        $content_result = 'お問い合わせ内容が入力されてません。';
+    } else {
+        $content_result = 'お問い合わせ内容:' . $content;
+    }
+
+    ?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>入力内容確認</title>
+</head>
+<body>
+<h1>入力内容確認</h1>
+<p><?php echo $nickname_result ?></p>
+<p><?php echo $email_result ?></p>
+<p><?php echo $content_result ?></p>
+</body>
+</html>
